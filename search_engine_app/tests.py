08bf61f860098fc616805f4df9b7c402ca58743b
adarsh koppa manjunath <adarshkoppamanjunath@gmail.com>
2020-01-29 01:43:40 -0600

    added test file which has google search library and results.html display results

--- search_engine_app/tests.py
+++ search_engine_app/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from googlesearch import search
+
+def search_web(topic):
+    web_result_list=[]
+    web_results=search(topic,lang="en",num=10,stop=10,pause=1)
+    for index in web_results:
+        web_result_list.append(index)
+    return web_result_list
+    
 
 # Create your tests here.
--- search_engine_app/views.py
+++ search_engine_app/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.shortcuts import render
 from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django import forms
+from search_engine_app.tests import search_web
 
 
 # Create your views here.
-
-def search(request):        
-    if request.method == 'GET': # this will be GET now      
-         return render(request,"home.html",{}) # do some research what it does
+class ContactForms(forms.Form):
+    name=forms.CharField(label = '')
+    
+def search(request):
+    if request.method=="POST":
+        form = ContactForms(request.POST)
+        if form.is_valid():
+            name=form.cleaned_data['name']
+            result=search_web(name)
+            return render(request,"results.html",{'result':result})
+    form = ContactForms()
+    return render(request,"home.html",{'form':form}) # do some research what it does
          
