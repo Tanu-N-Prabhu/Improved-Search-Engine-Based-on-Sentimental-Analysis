0075b715049dd92651448aad5a6ccf87a36a17b1
adarsh koppa manjunath <adarshkoppamanjunath@gmail.com>
2020-02-09 23:54:08 -0600

    extract web content func is added with the thread

--- search_engine_app/tests.py
+++ search_engine_app/tests.py
@@ -1,12 +1,69 @@
+#import logger
 from django.test import TestCase
 from googlesearch import search
+import requests
+import threading
+from bs4 import BeautifulSoup
+import urllib.request
+import urllib.error
+import time
+from multiprocessing import Pool
+from google import google
+
 
 def search_web(topic):
+    """This function takes input as any news topic and search for it in web and return website source and URL
+    Input:
+        topic- News topic user input
+    Output:
+        website- web source name
+        URL- web source URL"""
+
     web_result_list=[]
     web_results=search(topic,lang="en",num=10,stop=10,pause=1)
+    #web_results = google.search(topic,1)
     for index in web_results:
         web_result_list.append(index)
     return web_result_list
-    
+
+def extract_content(URL):
+    """This function takes URL as input and parse it to beautiful soup library to extract content and also clean it at the primary level with basic blacklist filter
+    Input:
+        URL- needs to be extracted
+    Output:
+        text- extracted text"""
+    res = requests.get(URL)
+    html_page = res.content
+    soup = BeautifulSoup(html_page, 'html.parser')
+    text = soup.find_all(text=True)
+    output = ''
+    blacklist = [
+	'[document]',
+	'noscript',
+	'header',
+	'html',
+	'meta',
+	'head', 
+	'input',
+	'script',
+	# there may be more elements you don't want, such as "style", etc.
+    ]
+    for t in text:
+        if t.parent.name not in blacklist:
+            output += '{} '.format(t)
+    output="".join([s for s in output.strip().splitlines(True) if s.strip()])
+    final_output.append("\n URL--%s \n Result--\n %s \n\n"%(URL,output[:1000]))
+    return output[:1000]
 
 # Create your tests here.
+
+def thread_func(topic):
+    web_result_list=search_web(topic)
+    global final_output
+    final_output=[]
+    threads = [threading.Thread(target=extract_content, args=(url,)) for url in web_result_list]
+    for thread in threads:
+      thread.start()
+    for thread in threads:
+      thread.join()
+    return final_output
--- search_engine_app/views.py
+++ search_engine_app/views.py
@@ -1,20 +1,20 @@
+#import logger
 from django.shortcuts import render
 from django.shortcuts import render
 from django.http import Http404, HttpResponse, HttpResponseRedirect
-from django import forms
-from search_engine_app.tests import search_web
+from search_engine_app.forms import ContactForms
+from search_engine_app.tests import thread_func
 
 
 # Create your views here.
-class ContactForms(forms.Form):
-    name=forms.CharField(label = '')
+
     
 def search(request):
     if request.method=="POST":
         form = ContactForms(request.POST)
         if form.is_valid():
             name=form.cleaned_data['name']
-            result=search_web(name)
+            result=thread_func(name)
             return render(request,"results.html",{'result':result})
     form = ContactForms()
     return render(request,"home.html",{'form':form}) # do some research what it does
