541bdbee5db306aaceeb436ad7cb2e7d43520411
Tanu Nanda Prabhu <49044332+Tanu-N-Prabhu@users.noreply.github.com>
2020-02-18 23:34:33 -0600

    Update forms.py
    Added the radio button form

--- search_engine_app/forms.py
+++ search_engine_app/forms.py
@@ -1,5 +1,16 @@
 from django import forms
 
+# The below is the radio buttons options for the sentiments
+sentimentOptions= [
+    ('positve', 'Postive'),
+    ('negative', 'Negative'),
+    ('na', 'N/A'),
+    
+    ]
 
 class ContactForms(forms.Form):
-    name=forms.CharField(label = '', widget=forms.TextInput(attrs={'placeholder':'  Search....'}))
+    name = forms.CharField(label = '', widget=forms.TextInput(attrs={'placeholder':'  Search....'}))
+
+
+class radioButtons(forms.Form):
+    options = forms.CharField(label='Please select the sentiment', widget=forms.RadioSelect(choices=sentimentOptions))
