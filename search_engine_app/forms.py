a4afbf762364bfe8a8591acf3472fe00204ac0e7
Tanu Nanda Prabhu <49044332+Tanu-N-Prabhu@users.noreply.github.com>
2020-02-25 14:23:03 -0600

    Update forms.py
    Updated the file by adding login form and Sign-up form

--- search_engine_app/forms.py
+++ search_engine_app/forms.py
@@ -8,9 +8,18 @@ sentimentOptions= [
     
     ]
 
-class ContactForms(forms.Form):
+class GeneralForms(forms.Form):
     name = forms.CharField(label = '', widget=forms.TextInput(attrs={'placeholder':'  Search....'}))
-
-
-class radioButtons(forms.Form):
     options = forms.CharField(label='Please select the sentiment', widget=forms.RadioSelect(choices=sentimentOptions))
+
+
+class loginForm(forms.Form):
+    uname = forms.CharField(label = 'Username', widget=forms.TextInput(attrs={'placeholder':' Username'}))
+    pwd = forms.CharField(label = 'Password', widget=forms.PasswordInput(attrs={'placeholder':' Password'}))
+
+
+class signUpForm(forms.Form):
+    newName = forms.CharField(label = 'Enter you name', widget=forms.TextInput(attrs={'placeholder':' Enter your name'}))
+    email = forms.CharField(label = 'Enter your e-mail address', widget = forms.TextInput(attrs = {'placeholder': ' Enter your E-mail address'}))
+    pwd = forms.CharField(label = 'Password', widget=forms.PasswordInput(attrs={'placeholder':' Enter a new password'}))
+    pwdr = forms.CharField(label = 'Password', widget=forms.PasswordInput(attrs={'placeholder':' Re-enter the password'}))
