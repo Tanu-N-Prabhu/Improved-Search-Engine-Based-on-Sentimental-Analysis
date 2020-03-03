39f7c25425a15ab0265bdfba1bc1cf1449a4cb5b
Tanu Nanda Prabhu <49044332+Tanu-N-Prabhu@users.noreply.github.com>
2020-03-02 22:42:27 -0600

    Update views.py
    Updated the views

--- search_engine_app/views.py
+++ search_engine_app/views.py
@@ -1,19 +1,24 @@
 import logging
 import traceback
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from search_engine_app.forms import GeneralForms
-from search_engine_app.forms import loginForm
+from search_engine_app.forms import userPortalForms
+#from search_engine_app.forms import loginForm
 from search_engine_app.search_web import SearchWeb
 from search_engine_project.logger import log
 from django.contrib import messages
-from search_engine_app.forms import signUpForm
+#from search_engine_app.forms import signUpForm
 from django.db import models
 from django.contrib.auth.models import User
 from search_engine_app.user_permissions import Authentication
 from search_engine_app.trending_topics import TrendingTopics
 # Create your views here.
 
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 def search(request):
     """This function communicates between template and search_web files"""
@@ -52,7 +57,7 @@ def ise(request):
 def projectTeam(request):
     return render(request, "projectTeam.html")
 
-
+'''
 def login(request):
     if request.method=="POST":
             
@@ -103,6 +108,41 @@ def signUp(request):
         
         
     return render(request,"signUp.html",{'form': form2}) 
+'''
+
+def registerPage(request):
+	form = CreateUserForm()
+
+	if request.method == 'POST':
+		form = CreateUserForm(request.POST)
+		if form.is_valid():
+			form.save()
+			uname = form.cleaned_data.get('username')
+			messages.success(request, 'Account was created for '+ uname )
+
+			return redirect('login')
+	context = {'form': form}
+	return render(request, 'register.html', context)
+
+def loginPage(request):
+
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+
+		user = authenticate(request, username = username, password = password )
+
+		if user is not None:
+			login(request, user)
+			return redirect('userPortal.html')
+
+	context = {}
+	return render(request, 'login.html', context)
+
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
 
 def trending(request):
     if request.method=="GET":
@@ -110,6 +150,25 @@ def trending(request):
         trending_res=trendingObj.trending_topics()
         trending_res={'trending_res':trending_res}
         return render(request,"trending.html",trending_res)
+
+
+def userPortal(request):
+    if request.method=="POST":
+            
+            form3 = userPortalForms(request.POST)
+
+            if form3.is_valid():
+                topic = form3.cleaned_data['topic']
+                country = form3.cleaned_data['country']
+                print(topic)
+                print(country)
+           
+            
+    form3 = userPortalForms()
+        
+        
+    return render(request,"userPortal.html",{'form': form3}) 
+
 
 
 
