17a3294781a931798865229420dbd357fb40ed38
Tanu Nanda Prabhu <49044332+Tanu-N-Prabhu@users.noreply.github.com>
2020-03-11 15:39:10 -0600

    Update settings.py

--- search_engine_project/settings.py
+++ search_engine_project/settings.py
@@ -123,7 +123,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT= os.path.join(BASE_DIR,'assets')
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
+
+
 
 #logging
 from .logger import LOGGING
