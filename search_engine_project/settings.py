e39cfa60165ee54ba29b50af50c26530485f30cc
Tanu Nanda Prabhu <49044332+Tanu-N-Prabhu@users.noreply.github.com>
2020-03-11 19:07:02 -0600

    Update settings.py

--- search_engine_project/settings.py
+++ search_engine_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 #import logger
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +133,4 @@ STATIC_ROOT= os.path.join(BASE_DIR,'assets')
 
 #logging
 from .logger import LOGGING
+django_heroku.settings(locals())
